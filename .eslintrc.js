0412d5aaeba3e5eb18e09bdf885202549335e0a1
dmitrygvl <dmitrygv1994@ya.ru>
2024-01-08 21:35:32 +0300

    Add env

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      'jest/globals': true,
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'jest'],
+    extends: [
+      'airbnb-base',
+      'plugin:jest/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: ['.eslintrc.{js,cjs}'],
+        parserOptions: {
+          sourceType: 'script',
+        },
+      },
+    ],
+    ignorePatterns: ['main.js'],
+    rules: {
+      'max-len': [
+        'error',
+        {
+          code: 140,
+          ignoreComments: true,
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': 0,
+      'no-param-reassign': ['error', { props: false }],
+      'import/prefer-default-export': 'off',
+      'import/no-unresolved': 'off',
+      'import/extensions': ['warn', 'never'],
+    },
+  };
